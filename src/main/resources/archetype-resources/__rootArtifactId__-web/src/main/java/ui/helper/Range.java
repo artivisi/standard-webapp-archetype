07332d93fa343ef44606b6a066acf5869b593953
Endy Muhardin <endy.muhardin@gmail.com>
2011-12-08 12:02:08 +0700

    fix perhitungan range

--- src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/java/ui/helper/Range.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/java/ui/helper/Range.java
@@ -28,33 +28,33 @@ public class Range {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(Range.class);
 	private static final Integer DEFAULT_FROM = 0;
-	private static final Integer DEFAULT_TO = 20;
-	
+	private static final Integer DEFAULT_TO = 19;
+
 	private Integer from = DEFAULT_FROM;
 	private Integer to = DEFAULT_TO;
 	private Long total;
-	
+
 	public static Range fromRequestHeader(String strRange){
 		Range result = new Range();
-		
+
 		if(!StringUtils.hasText(strRange)) {
 			return result;
 		}
-		
+
 		if(!strRange.startsWith("items")) {
 			return result;
 		}
-		
+
 		String[] rangeContent = strRange.split("=");
 		if(rangeContent.length != 2) {
 			return result;
 		}
-		
+
 		String[] rangeValue = rangeContent[1].split("-");
 		if(rangeValue.length != 2) {
 			return result;
 		}
-		
+
 		try {
 			Integer f = Integer.valueOf(rangeValue[0].trim());
 			Integer t = Integer.valueOf(rangeValue[1].trim());
@@ -64,10 +64,10 @@ public class Range {
 			LOGGER.info("Invalid range value [{}] - [{}]", rangeValue[0], rangeValue[1]);
 			return result;
 		}
-		
+
 		return result;
 	}
-	
+
 	public String toResponseHeader(){
 		if(from == null || to == null || from < 0 || to < 0) {
 			return "";
@@ -75,19 +75,19 @@ public class Range {
 		String output = "items "+getFrom()+"-"+getTo();
 		return total == null ? output : output+"/"+getTotal();
 	}
-	
+
 	public Range() {
 	}
-	
+
 	public Range(Integer from, Integer to, Long total) {
 		this.from = from;
 		this.to = to;
 		this.total = total;
-		
+
 		if(total != null && total < to) {
-			this.to = total.intValue();
+			this.to = total.intValue() - 1;
 		}
-		
+
 	}
 
 
@@ -109,8 +109,8 @@ public class Range {
 	public void setTotal(Long total) {
 		this.total = total;
 	}
-	
-	
+
+
 	@Override
 	public int hashCode() {
 		return Objects.hashCode(from, to, total);
@@ -130,6 +130,6 @@ public class Range {
 	public String toString() {
 		return "{from:" + from + ", to:" + to + ", total:" + total + "}";
 	}
-	
-	
+
+
 }
